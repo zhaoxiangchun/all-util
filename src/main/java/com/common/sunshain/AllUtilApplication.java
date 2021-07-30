df4c7f2d77f486e36b7c1b69cc5572bacef5cfe1
zhaoxiangchun <1422928955@qq.com>
2021-07-30 18:54:38 +0800

    init

---
+++ src/main/java/com/common/sunshain/AllUtilApplication.java
@@ -0,0 +1,13 @@
+package com.common.sunshain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllUtilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AllUtilApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/common/sunshain/AllUtilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.common.sunshain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AllUtilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
